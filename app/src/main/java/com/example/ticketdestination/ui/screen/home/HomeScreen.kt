d27b4bb32a387617ea5130e45ee57a19a96ec8f6
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2023-05-23 18:54:39 +0700

    Menambahkan fitur Home

--- app/src/main/java/com/example/ticketdestination/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/ticketdestination/ui/screen/home/HomeScreen.kt
@@ -1,9 +1,20 @@
 package com.example.ticketdestination.ui.screen.home
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.ticketdestination.ViewModelFactory
 import com.example.ticketdestination.di.Injection
+import com.example.ticketdestination.model.OrderTicket
+import com.example.ticketdestination.ui.common.UiState
+import com.example.ticketdestination.ui.components.TicketItem
 
 @Composable
 fun HomeScreen(
@@ -13,5 +24,45 @@ fun HomeScreen(
     ),
     navigateToDetail: (Long) -> Unit
 ) {
+    viewModel.uiState.collectAsState(initial = UiState.Loading).value.let {
+        when (it) {
+            is UiState.Loading -> {
+                viewModel.getAllTicket()
+            }
+            is UiState.Success -> {
+                HomeContent(
+                    buyTicket = it.data ,
+                    modifier = modifier,
+                    navigateToDetail = navigateToDetail,
+                )
+            }
+            is UiState.Error -> {}
+        }
+    }
+}
 
+@Composable
+fun HomeContent(
+    buyTicket: List<OrderTicket>,
+    modifier: Modifier = Modifier,
+    navigateToDetail: (Long) -> Unit,
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(160.dp) ,
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier
+    ) {
+        items(buyTicket) { data ->
+            TicketItem(
+                photoUrl = data.ticket.photoUrl ,
+                title = data.ticket.title,
+                price = data.ticket.priceTicket,
+                modifier = Modifier.clickable {
+                    navigateToDetail(data.ticket.id)
+                }
+            )
+        }
+    }
 }
